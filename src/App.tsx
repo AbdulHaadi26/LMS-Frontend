de294bf0e04e0e01473b05ba2e770f2b90f6f83b
Abdul Haadi <57644193+AbdulHaadi26@users.noreply.github.com>
2024-10-20 20:03:40 +0500

    Init Repo

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FluidContainer } from "./components/containers";
+
+function App() {
+  return (
+    <FluidContainer className="items-center justify-center">
+      Learning Management System
+    </FluidContainer>
+  );
+}
+
+export default App;
