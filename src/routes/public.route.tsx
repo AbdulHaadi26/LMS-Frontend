93328af59220a9cf3eea894c7e9d97689a87ada4
Abdul Haadi <57644193+AbdulHaadi26@users.noreply.github.com>
2024-10-21 23:25:58 +0500

    Added Thunk

--- src/routes/public.route.tsx
+++ src/routes/public.route.tsx
@@ -1,10 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import RegisterPage from "../pages/register.page";
 
 const PublicRoute = () => {
   return (
     <Routes>
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="*" element={<Navigate to="/register" />} />
     </Routes>
   );
 };
